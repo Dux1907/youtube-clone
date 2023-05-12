3cdaf8a29aa7784fa8110dbc33ac8a9da629a54e
Dux <kartikmadaan1907@gmail.com>
2023-05-13 02:26:30 +0530

    Channel details

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import Navbar from "./assets/Navbar";
 import { Routes, Route } from "react-router-dom";
-import { Feed } from "./assets/pages";
+import { Feed }  from "./assets/pages";
 import VideoDetail from "./assets/VideoDetail";
+import ChannelDetail from './assets/ChannelDetail'
 const App = () => {
   return (
     <>
@@ -9,6 +10,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Feed />}></Route>
         <Route path="/video/:videoId" element={<VideoDetail />}></Route>
+        <Route path = '/channel/:channelId' element = {<ChannelDetail/>}></Route>
       </Routes>
     </>
   );
