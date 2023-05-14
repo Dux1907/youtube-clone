8aebd9b16a621ae22be82a502ae029cd8abcc093
kartik <kartikmadaan1907@gmail.com>
2023-05-15 00:35:57 +0530

    video comments

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import { Feed }  from "./assets/pages";
 import VideoDetail from "./assets/VideoDetail";
 import ChannelDetail from './assets/ChannelDetail'
+import SearchDetail from "./assets/SearchDetail";
 const App = () => {
   return (
     <>
@@ -10,6 +11,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Feed />}></Route>
         <Route path="/video/:videoId" element={<VideoDetail />}></Route>
+        <Route path="/search/:searchId" element={<SearchDetail />}></Route>
         <Route path = '/channel/:channelId' element = {<ChannelDetail/>}></Route>
       </Routes>
     </>
